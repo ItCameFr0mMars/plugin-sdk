9e3acd6dbd60e31d27a12badddf994898baf237e
ItCameFr0mMars <ryanmertz06@Gmail.com>
2021-12-03 17:01:59 +0000

    implement constructor's warnings

--- src/main/kotlin/StocksHud.kt
+++ src/main/kotlin/StocksHud.kt
@@ -1,10 +1,14 @@
 import com.google.gson.Gson
+import com.lambda.client.LambdaMod
 import com.lambda.client.util.TickTimer
 import com.lambda.client.util.TimeUnit
 import com.lambda.client.util.WebUtils
 import com.lambda.client.util.text.MessageSendHelper
 import com.lambda.client.event.SafeClientEvent
 import com.lambda.client.plugin.api.PluginLabelHud
+import com.lambda.client.util.threads.defaultScope
+import kotlinx.coroutines.launch
+import kotlin.properties.Delegates
 
 internal object StocksHud: PluginLabelHud(
     name = "Stocks",
@@ -13,59 +17,59 @@ internal object StocksHud: PluginLabelHud(
     pluginMain = PluginStocks
 ) {
     private var symbol by setting("Symbol", "TSLA")
-    private val tickdelay by setting("Delay", 30, 20..120, 1)
+    private val tickdelay by setting("Delay", 30, 5..120, 1)
     private var token by setting("Token", "Set your token with the command ;set Stocks Token (token)")
     private val ticktimer = TickTimer(TimeUnit.SECONDS)
     private var url = "https://finnhub.io/api/v1/quote?symbol=$symbol&token=$token"
     private var stockData = StockData(0.0)
     private var price = 0.0
-    private var prevprice = 0.0
+    private var prevprice by Delegates.notNull<Double>()
     private var sentwarning = false
     private var color = secondaryColor
 
 
     override fun SafeClientEvent.updateText() {
-        if (sentwarning == false) {
+        if (!sentwarning) {
             sendWarning()
         }
 
-        if (ticktimer.tick(tickdelay)) {
-            updateStockData()
+        if (ticktimer.tick(tickdelay) && !token.startsWith("Set")) {
+            defaultScope.launch {
+                try {
+                    url = "https://finnhub.io/api/v1/quote?symbol=${symbol.uppercase()}&token=$token"
+                    price = Gson().fromJson(WebUtils.getUrlContents(url), StockData::class.java).c
+                } catch (e: Exception) {
+                    LambdaMod.LOG.error("Failed to connect to finnhub api", e)
+                    MessageSendHelper.sendWarningMessage("[StockTicker] Token not accepted: $token")
+                }
+            }
         }
-        displayText.add("Current Price of ${symbol.toUpperCase()} is", primaryColor)
-        if (prevprice > price) {
+        displayText.clear()
+        displayText.add("Current Price of ${symbol.uppercase()} is", primaryColor)
+        if (prevprice < price) {
             color.r = 0
             color.g = 255
             color.b = 0
             color.a = 255
             displayText.add("$price", color)
-        } else {
+        } else if (prevprice > price ) {
             color.r = 255
             color.g = 0
             color.b = 0
             color.a = 255
             displayText.add("$price", color)
-        }
-        if (ticktimer.tick(2)) {
+        } else {
+            displayText.add("$price", secondaryColor)
+        }; if (ticktimer.tick(2)) {
             displayText.add("$price", secondaryColor)
             prevprice = price
-            color = secondaryColor
-        }
-    }
-
-    private fun updateStockData() {
-        if (token.length != 20) {
-
-        } else {
-            url = "https://finnhub.io/api/v1/quote?symbol=${symbol.toUpperCase()}&token=$token"
-            price = Gson().fromJson(WebUtils.getUrlContents(url), StockData::class.java).c
         }
     }
 
 
     private fun sendWarning() {
         MessageSendHelper.sendWarningMessage(
-            "This module uses an external API, finnhub.io, which requires an api token to use. " +
+            "[StockTicker] This module uses an external API, finnhub.io, which requires an api token to use. " +
                 "Go to https://finnhub.io/dashboard and sign up for an account and copy your api token. " +
                 "Once you have gotten your api token, you can run this command: " +
                 ";set Stocks Token (paste token here)"
